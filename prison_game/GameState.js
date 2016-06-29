3615fe05a9b16cfd26dd42c18ca70820e09c1c04
bobowitz <bobowitzbob@gmail.com>
2016-06-29 11:38:35 -0700

    started prison code

---
+++ prison_game/GameState.js
@@ -0,0 +1,17 @@
+var Game = {};
+
+Game.GameState = function (game) {
+
+};
+
+Game.GameState.prototype = {
+
+    preload: function () {
+    	game.scale.pageAlignHorizontally = true;
+        game.scale.pageAlignVertically = true;
+    },
+
+    create: function () {
+    	
+    }
+};
